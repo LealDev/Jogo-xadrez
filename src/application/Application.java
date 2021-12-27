38040b49ebd131ab13ea0ef262dc739ad87a7046
LealDev <lealmaster0111@gmail.com>
2021-12-27 18:13:26 -0300

    second commit ;)

--- src/application/Application.java
+++ src/application/Application.java
@@ -1,7 +1,14 @@
 package application;
 
+import boardGame.Position;
+
 public class Application {
+	
 	public static void main(String [] args) {
+		
+		Position pos = new Position(3, 5);
+		
+		System.out.println(pos);
 		
 	}
 }
